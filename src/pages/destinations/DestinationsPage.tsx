406017ff2606bcb985edd6d3fd0556634662d7da
DomasPauliukas <Domas0319@gmail.com>
2025-04-23 10:20:51 +0300

    done

--- src/pages/destinations/DestinationsPage.tsx
+++ src/pages/destinations/DestinationsPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom"; // 👈 Turi būti kartu su React importais
+import { Link, useNavigate } from "react-router-dom";
 import { getDestinations } from "../../utils/api";
 
 interface Destination {
